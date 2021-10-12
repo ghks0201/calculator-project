36ca2affd8dc8490cd71499bcbe7d2a8aaf7b424
ghks0201 <34975888+ghks0201@users.noreply.github.com>
2021-10-12 12:00:46 +0800

    Change behaviour and apperance of calculator

--- calculator.js
+++ calculator.js
@@ -2,11 +2,16 @@ const numberKeys = document.querySelectorAll(".number");
 const operateKeys = document.querySelectorAll(".operator")
 const display = document.querySelector(".display");
 const equalKey = document.querySelector(".equal");
+const clearKey = document.querySelector(".clear");
+const percentKey = document.querySelector(".percentage");
+const backspaceKey = document.querySelector(".backspace");
 
-let tempValue, firstValue = 0;
+let tempValue = 0;
+let firstValue = 0;
 let operatorSymbol = "";
 let hasOperator = false;
 let hasTotal = false;
+let initialDisplay = true;
 
 
 function add(num1, num2) {
@@ -22,7 +27,11 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 === 0) {
+        const errorMsg = "Error!";
+        return errorMsg;
+    }
+    return Math.round(num1 / num2 * 100) / 100;
 };
 
 function operate(operatorFunc, num1, num2) {
@@ -42,9 +51,19 @@ numberKeys.forEach(key => {
             hasOperator = false;
             hasTotal = false;
         }
+
+        if ((display.textContent == 0) && initialDisplay) {
+            display.textContent = "";
+            initialDisplay = false; 
+        }
+
+        if (display.textContent.length > 12) {
+            return;
+        }
         
         display.textContent += key.textContent;
-        tempValue = parseInt(display.textContent);
+        tempValue = Number(display.textContent);
+        console.log(tempValue)
     });
 
 });
@@ -52,16 +71,72 @@ numberKeys.forEach(key => {
 operateKeys.forEach(key => {
     
     key.addEventListener("click", () => {
-        firstValue = tempValue;
+        if (firstValue && tempValue) {
+            firstValue = operate(operatorSymbol, firstValue, tempValue)
+            console.log(firstValue)
+        }
+        else {
+            firstValue = tempValue;
+            tempValue = 0;
+        }
+        
         operatorSymbol = key.classList.item(2);
         hasOperator = true;
     })
 
 })
 
-equalKey.addEventListener("click", () => {
-    operate(operatorSymbol, firstValue, tempValue)
+percentKey.addEventListener("click", () => {
 
-    hasTotal = true
-    firstValue, tempValue = 0
+    if (tempValue === 0) {
+        display.textContent = 0;
+    }
+
+    else {
+        tempValue /= 100;
+        display.textContent = tempValue;
+        return tempValue;
+    }
+    
+});
+
+equalKey.addEventListener("click", () => {
+
+    if (!tempValue && !operatorSymbol) {
+        display.textContent = 0;
+        return;
+    };
+
+    let total = operate(operatorSymbol, firstValue, tempValue);
+    
+    console.log(total);
+
+    hasTotal = true;
+    tempValue = total;
+    firstValue = 0;
+    return total;
+
+})
+
+clearKey.addEventListener("click", () => {
+
+    firstValue = 0;
+    tempValue = 0;
+    operatorSymbol = "";
+    display.textContent = "0";
+    initialDisplay = true;
+
+})
+
+backspaceKey.addEventListener("click", () => {
+
+    tempValue = Number(tempValue.toString().slice(0, -1))
+    display.textContent = tempValue
+
+    if (tempValue === 0) {
+        initialDisplay = true
+    }
+
+    return tempValue
+
 })
